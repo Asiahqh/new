7a661f8c5cb84d6b94526d486ae23d275f489206
Asiahqh <asiah1416@hotmail.com>
2024-05-17 14:24:17 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,79 @@
+﻿define({ entries : {
+    "bhattacharya2023goblin": {
+        "author": "Bhattacharya, Kunal and Nandi, Anjan K",
+        "journal": "Available at SSRN 4476837",
+        "title": "Goblin's Challenge to ChatGPT: Exploring AI's Dilemma Resolution and Mentalization through Riddle Tales",
+        "type": "article",
+        "year": "2023"
+    },
+    "church2024emerging": {
+        "author": "Church, Kenneth",
+        "journal": "Natural Language Engineering",
+        "pages": "1--11",
+        "publisher": "Cambridge University Press",
+        "title": "Emerging trends: When can users trust GPT, and when should they intervene?",
+        "type": "article",
+        "year": "2024"
+    },
+    "elkhatat2023evaluating": {
+        "author": "Elkhatat, Ahmed M",
+        "journal": "International Journal for Educational Integrity",
+        "number": "1",
+        "pages": "15",
+        "publisher": "Springer",
+        "title": "Evaluating the authenticity of ChatGPT responses: a study on text-matching capabilities",
+        "type": "article",
+        "volume": "19",
+        "year": "2023"
+    },
+    "greshake2023not": {
+        "author": "Greshake, Kai and Abdelnabi, Sahar and Mishra, Shailesh and Endres, Christoph and Holz, Thorsten and Fritz, Mario",
+        "booktitle": "Proceedings of the 16th ACM Workshop on Artificial Intelligence and Security",
+        "pages": "79--90",
+        "title": "Not what you've signed up for: Compromising real-world llm-integrated applications with indirect prompt injection",
+        "type": "inproceedings",
+        "year": "2023"
+    },
+    "johnson2023assessing": {
+        "author": "Johnson, Douglas and Goodman, Rachel and Patrinely, J and Stone, Cosby and Zimmerman, Eli and Donald, Rebecca and Chang, Sam and Berkowitz, Sean and Finn, Avni and Jahangir, Eiman and others",
+        "journal": "Research square",
+        "publisher": "American Journal Experts",
+        "title": "Assessing the accuracy and reliability of AI-generated medical responses: an evaluation of the Chat-GPT model",
+        "type": "article",
+        "year": "2023"
+    },
+    "laskar2023systematic": {
+        "author": "Laskar, Md Tahmid Rahman and Bari, M Saiful and Rahman, Mizanur and Bhuiyan, Md Amran Hossen and Joty, Shafiq and Huang, Jimmy Xiangji",
+        "journal": "arXiv preprint arXiv:2305.18486",
+        "title": "A systematic study and comprehensive evaluation of chatgpt on benchmark datasets",
+        "type": "article",
+        "year": "2023"
+    },
+    "lin2021truthfulqa": {
+        "author": "Lin, Stephanie and Hilton, Jacob and Evans, Owain",
+        "journal": "arXiv preprint arXiv:2109.07958",
+        "title": "Truthfulqa: Measuring how models mimic human falsehoods",
+        "type": "article",
+        "year": "2021"
+    },
+    "mcintosh2023culturally": {
+        "author": "McIntosh, Timothy R and Liu, Tong and Susnjak, Teo and Watters, Paul and Ng, Alex and Halgamuge, Malka N",
+        "journal": "IEEE Transactions on Artificial Intelligence",
+        "publisher": "IEEE",
+        "title": "A culturally sensitive test to evaluate nuanced gpt hallucination",
+        "type": "article",
+        "year": "2023"
+    },
+    "patilexploratory": {
+        "author": "Patil, Ravindra",
+        "title": "AN EXPLORATORY STUDY ON PROBLEMS \\& CHALLENGES FACED BY USERS OF CHATGPT",
+        "type": "article"
+    },
+    "wang2023decodingtrust": {
+        "author": "Wang, Boxin and Chen, Weixin and Pei, Hengzhi and Xie, Chulin and Kang, Mintong and Zhang, Chenhui and Xu, Chejian and Xiong, Zidi and Dutta, Ritik and Schaeffer, Rylan and others",
+        "journal": "arXiv preprint arXiv:2306.11698",
+        "title": "Decodingtrust: A comprehensive assessment of trustworthiness in gpt models",
+        "type": "article",
+        "year": "2023"
+    }
+}});
